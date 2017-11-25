0b13cc88a3e5635dda4decbec57173960621bfc5
Brett Mandler <brett@loudway.com>
2017-11-24 22:27:18 -0500

    Added unit test for deck.go

---
+++ deck_test.go
@@ -0,0 +1,15 @@
+package pokerbot
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDeck(t *testing.T) {
+	deck := NewDeck()
+	assert.Equal(t, len(deck.cards), 52, "The deck was not successfully created")
+	cards := deck.Draw(5)
+	assert.Equal(t, len(cards), 52-len(deck.cards), "Cards were not removed from the deck")
+	deck.Shuffle(cards...)
+	assert.Equal(t, len(deck.cards), 52, "Cards were not added back to the deck")
+}
