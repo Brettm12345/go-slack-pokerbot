3dcc3883eed33d091d636fb50e26e61d81ef0b96
Brett Mandler <brett@loudway.com>
2017-11-24 22:27:50 -0500

    Fixed card.go

--- card.go
+++ card.go
@@ -1,19 +1,22 @@
 package pokerbot
 
-import s "strings"
+import "strings"
 
 // Card represents a playing card in a deck.
 type Card struct {
 	suite string
 	rank  string
-	image string
 }
 
-func (c Card) String() string {
+func (c Card) message() string {
 	suiteEmoji := map[string]string{"Spades": "♠", "Hearts": "♥", "Diamonds": "♦", "Clubs": "♣"}
-	return c.rank + suiteEmoji[s.Title(c.suite)]
+	return c.rank + suiteEmoji[strings.Title(c.suite)]
 }
 
 func (c Card) ascii() string {
-	return c.rank + s.Title(c.suite)
+	return c.rank + " " + strings.Title(c.suite)
+}
+
+func (c Card) image() string {
+	return strings.Join([]string{"cards/", strings.Title(c.rank), c.suite[:1], ".png"}, "")
 }
