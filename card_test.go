67b0b18fe41fe5f031186fab2e7920ed04c88bd9
Brett Mandler <brett@loudway.com>
2017-11-24 22:27:02 -0500

    Added unit tests for card.go

--- card_test.go
+++ card_test.go
@@ -1,1 +1,13 @@
 package pokerbot
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCard(t *testing.T) {
+	card := Card{rank: "A", suite: "spades"}
+	assert.Equal(t, card.message(), "A♠", "Card string conversion error")
+	assert.Equal(t, card.ascii(), "A Spades", "Card string conversion error")
+	assert.Equal(t, card.image(), "cards/As.png", "Failed to get card image")
+}
